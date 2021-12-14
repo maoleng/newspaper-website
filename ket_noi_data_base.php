10b3a5fce200dc3df9ba6251b2782598e3914f7a
Bùi Hữu Lộc <91431461+maoleng@users.noreply.github.com>
2021-12-15 02:11:20 +0700

    newspaper web

--- ket_noi_data_base.php
+++ ket_noi_data_base.php
@@ -1,6 +1,6 @@
 <?php 
 
-$ket_noi_database = mysqli_connect('localhost', 'feature45', '', 'tin_tuc');
+$ket_noi_database = mysqli_connect('localhost', 'root', '', 'tin_tuc');
 mysqli_set_charset($ket_noi_database ,'utf8');
 
  ?>
